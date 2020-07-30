0a68e0ceb7b5d897df60426d041abe0959551614
Mansi <43803851+mansimm@users.noreply.github.com>
2020-07-30 19:07:22 +0530

    Python If-Else

--- Python/Introduction/if_else.py
+++ Python/Introduction/if_else.py
@@ -9,12 +9,13 @@ import sys
 
 
 if __name__ == '__main__':
-    n = int(raw_input().strip())
-    if n%2 ==1:
-        print "Weird"
+    n = int(input().strip())
+
+    if n%2 == 1:
+        print("Weird")
     elif n in range(2,6):
-        print "Not Weird"
+        print("Not Weird")
     elif n in range(6,21):
-        print "Weird"
+        print("Weird")
     else:
-        print "Not Weird"
+        print("Not Weird")
