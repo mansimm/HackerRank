d73c20e3a7c075dfb4f54dc72aec2679615d451c
Mansi <43803851+mansimm@users.noreply.github.com>
2021-07-25 10:57:12 +0530

    BigInteger problem solved

---
+++ Java/BigNumber/Java BigInteger/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        BigInteger x = sc.nextBigInteger();
+        BigInteger y = sc.nextBigInteger();
+        System.out.println(x.add(y));
+        System.out.println(x.multiply(y));
+    }
+}
